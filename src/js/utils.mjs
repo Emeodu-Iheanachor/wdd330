653a8e734095a4659ced7788a76295517eb69554
Emeodu-Iheanachor <iheralchi123@gmail.com>
2026-07-28 12:47:13 +0100

    Another update

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,57 +1,3 @@
-// wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
-
-// retrieve data from localstorage
-export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-// save data to local storage
-export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
-
-// get the product id from the query string
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param);
-  return product
-}
-
-export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(template);
-  // if clear is true we need to clear out the contents of the parent.
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
-
-export function renderWithTemplate(template, parentElement, data, callback) {
-  parentElement.innerHTML = template;
-  if (callback) {
-    callback(data);
-  }
-}
-
-async function loadTemplate(path) {
-  const res = await fetch(path);
-  const template = await res.text();
-  return template;
-}
-
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("../partials/header.html");
   const footerTemplate = await loadTemplate("../partials/footer.html");
@@ -61,4 +7,23 @@ export async function loadHeaderFooter() {
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
+
+  // Set paths after the header has been inserted into the page.
+  const base = import.meta.env.BASE_URL;
+
+  const homeLink = document.getElementById("site-home");
+  const cartLink = document.getElementById("cart-link");
+  const logo = document.getElementById("site-logo");
+
+  if (homeLink) {
+    homeLink.href = `${base}index.html`;
+  }
+
+  if (cartLink) {
+    cartLink.href = `${base}cart/index.html`;
+  }
+
+  if (logo) {
+    logo.src = `${base}images/noun_Tent_2517.svg`;
+  }
 }
