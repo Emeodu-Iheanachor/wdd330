e5336ba13a7a588ea2da969ff27cb09c9ad90457
Emeodu-Iheanachor <iheralchi123@gmail.com>
2026-07-29 18:08:39 +0100

    The first update for ERCI--individual4

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,20 +1,12 @@
 import { loadHeaderFooter, getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
 const category = getParam("category");
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
 listing.init();
-
-const title = document.querySelector(".title");
-
-if (title && category) {
-  title.textContent = `Top Products: ${category
-    .replace("-", " ")
-    .replace(/\b\w/g, (letter) => letter.toUpperCase())}`;
-}
--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,14 @@ export default defineConfig(({ mode }) => ({
 
   build: {
     outDir: "../dist",
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkedout: resolve(__dirname, "src/checkout/success.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        listing: resolve(__dirname, "src/product-listing/index.html"),
       },
     },
   },
